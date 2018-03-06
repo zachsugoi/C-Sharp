bcef85aa64a329879682319386042e142742c62c
Zach Gee <Zachsugoi@gmail.com>
2018-03-06 09:43:26 -0800

    added Music_LINQ and Wizard_Ninja_Samurai

---
+++ Music_LINQ/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JsonData;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+                //Collections to work with
+            List<Artist> Artists = JsonToFile<Artist>.ReadJson();
+            List<Group> Groups = JsonToFile<Group>.ReadJson();
+
+                //Grab all artists from Mount Vernon
+            var mtVernon =  from match in Artists
+                            where match.Hometown == "Mount Vernon"
+                            select new { match.ArtistName, match.RealName, match.Age };
+            foreach(var result in mtVernon){
+                Console.WriteLine("Artists from Mount Vernon");
+                Console.WriteLine("Artist: "+result.ArtistName);
+                Console.WriteLine("Artist's real name: "+result.RealName);
+                Console.WriteLine("Age: "+result.Age);
+            }
+
+                //Grab the youngest artist
+            int youngest_age = Artists.Select( age => new { age.Age }).Min( num => num.Age);  //The Select clause is unnecessary--just including it for reference
+            var youngest_artist = Artists.Where(artist => artist.Age == youngest_age);
+            Console.WriteLine("Youngest Artist");
+            foreach(var artist in youngest_artist){
+                Console.WriteLine($"Artist: {artist.ArtistName}");
+                Console.WriteLine($"Artist's real name: {artist.RealName}");
+                Console.WriteLine($"Age: {artist.Age}");
+            }
+            
+                //Display all artists with 'William' somewhere in their real name
+            var williams = Artists.Where(william => william.RealName.IndexOf("William") != -1);
+            Console.WriteLine("The Williams");
+            foreach(var artist in williams){
+                Console.WriteLine($"Artist: {artist.ArtistName}");
+                Console.WriteLine($"Artist's real name: {artist.RealName}");
+            }
+
+                //Display the 3 oldest artists from Atlanta
+            var old_atlanta = Artists.Where(atl => atl.Hometown =="Atlanta").OrderByDescending(age => age.Age).Take(3);
+            Console.WriteLine("Oldest Three Artists from Atlanta");
+            foreach(var artist in old_atlanta){
+                Console.WriteLine($"Artist: {artist.ArtistName}");
+                Console.WriteLine($"Artist's real name: {artist.RealName}");
+                Console.WriteLine($"Age: {artist.Age}");
+            }
+
+                //Display the Group Name of all groups that have members that are not from New York City
+            var groups_no_nyc = Artists.Where(nonyc => nonyc.Hometown != "New York City").Join(Groups,
+                                                                                                artist_group => artist_group.GroupId,
+                                                                                                group => group.Id,
+                                                                                                (artist_group, group) =>
+                                                                                                    new { group.GroupName }
+                                                                                                ).Distinct();
+            Console.WriteLine("Groups with no Members from NYC");
+            foreach(var group in groups_no_nyc){
+                Console.WriteLine($"Group Name: {group.GroupName}");
+            }
+
+                //Display the artist names of all members of the group 'Wu-Tang Clan'
+            var wutang = Groups.Where(clan => clan.GroupName =="Wu-Tang Clan").Join(Artists,
+                                                                                    group => group.Id,
+                                                                                    artist_grp => artist_grp.GroupId,
+                                                                                    (group, artist_grp) =>
+                                                                                        new { group.GroupName, artist_grp.ArtistName }
+                                                                                    );
+            Console.WriteLine("Wu-Tang Clan members");
+            foreach(var mbr in wutang){
+                Console.WriteLine($"Group name: {mbr.GroupName}");
+                Console.WriteLine($"Artist name: {mbr.ArtistName}");
+            }
+        }
+    }
+}
---
+++ Wizard_Ninja_Samurai/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Wizard_Ninja_Samurai
+{
+    //Create parent Human class
+    public class Human //get rid of all the _health_max stuff. can't get it to work.
+    {
+        public string name;
+        protected int _health;
+        public int health{
+            get { return _health; }
+            set {
+                _health = Math.Max(value, 0);
+             }
+        }
+        public int strength { get; set; }
+        public int intelligence { get; set; }
+        public int dexterity { get; set; }
+
+        public Human(string person){
+            name = person;
+            strength = 3;
+            intelligence = 3;
+            dexterity = 3;
+            health = 100;
+        }
+        public Human(string person, int str, int intel, int dex, int hp){
+            name = person;
+            strength = str;
+            intelligence = intel;
+            dexterity = dex;
+            health = hp;
+        }
+        public void Attack(object obj){
+            Human enemy = obj as Human;
+            if(enemy == null){
+                Console.WriteLine("Failed Attack");
+            }
+            else{
+                enemy.health -= strength*5;
+            }
+        }
+    }
+
+    public class Wizard : Human
+    {
+        public Wizard(string person) : base(person){
+            intelligence = 25;
+            health = 50;
+        }
+        public void Heal(){
+            health *= intelligence*10;
+        }
+        public void Fireball(object obj){
+            Random rand = new Random();
+            Human enemy = obj as Human;
+            if(enemy == null){
+                Console.WriteLine("Fireball failed!");
+            }
+            else{
+                enemy.health -= rand.Next(20,51);
+                Console.WriteLine($"{name} used fireball on {enemy.name}!");
+            }
+        }
+    }
+
+    public class Ninja : Human
+    {
+        public Ninja(string person) : base(person){
+            dexterity = 175;
+        }
+        public void Steal(object obj){
+            Human enemy = obj as Human;
+            if(enemy == null){
+                Console.WriteLine("Steal failed!");
+            }
+            else{
+                enemy.health -= 10;
+                health += 10;
+            }
+        }
+        public void Get_away(){
+            health -= 15;
+            Console.WriteLine("You got away!");
+        }
+    }
+
+    public class Samurai : Human
+    {
+        public static int count = 0;
+        //new public int health{
+        //    get { return _health; }
+        //    set {
+        //        _health = Math.Min(Math.Max(value, 0), _health_max);
+        //    }
+        //}
+        public Samurai(string person) : base(person){
+            count++;
+            health = 200;
+        }
+        public void Death_blow(object obj){
+            Human enemy = obj as Human;
+            if(enemy is null){
+                Console.WriteLine("Death blow failed!");
+            }
+            else{
+                if(enemy.health >= 50){
+                    Console.WriteLine("This guy is too strong for death blow!");
+                }
+                else{
+                    enemy.health = 0;
+                }
+            }
+        }
+        public void Meditate(){
+            health = 200;
+        }
+        public void How_many(){
+            Console.WriteLine($"There are {count} samurai.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Wizard zach = new Wizard("Zachard");
+            Console.WriteLine($"Intelligence: {zach.intelligence}");
+            Console.WriteLine($"Name: {zach.name}");
+            Console.WriteLine($"Dexterity: {zach.dexterity}");
+            Samurai kyle = new Samurai("Kylai");
+            Samurai alex = new Samurai("Alexai");
+            alex.How_many();
+            Human paul = new Human("Paul");
+            zach.Fireball(kyle);
+            Console.WriteLine($"Kyle's health is now {kyle.health}");
+            zach.Fireball(kyle);
+            Console.WriteLine($"Kyle's health is now {kyle.health}");
+            zach.Fireball(kyle);
+            Console.WriteLine($"Kyle's health is now {kyle.health}");
+            zach.Fireball(kyle);
+            Console.WriteLine($"Kyle's health is now {kyle.health}");
+        }
+    }
+}
